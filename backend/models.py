3efc91345fe68a43fd76e5e944f3b85c0ca577c6
Ader Konstantin <kot@aderci.ru>
2024-02-13 22:06:10 -0500

    created class Comment

--- backend/models.py
+++ backend/models.py
@@ -10,3 +10,16 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.CharField(max_length=256)
+    email = models.EmailField(null=True)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+       return self.author
